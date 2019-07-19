3d804f6e8088b9fc8b6a711b6194ff07a7c4f8ca
Gabriel Carvalho <36541377+Gamebielo@users.noreply.github.com>
2019-07-19 09:42:56 -0300

    Verificador de idade com imagens

---
+++ Verificador de Idade/script.js
@@ -0,0 +1,50 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear() // Pegar o ano completo, sem abreviação
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano) {
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'foto-crianca-m.png')
+            } else if (idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-m.png')
+            } else if (idade < 55) {
+                // ADULTO
+                img.setAttribute('src', 'foto-adulto-m.png')
+            } else {
+                // IDOSO
+                img.setAttribute('src', 'foto-idoso-m.png')
+            }
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // CRIANÇA
+                img.setAttribute('src', 'foto-crianca-f.png')
+            } else if (idade < 21) {
+                // JOVEM
+                img.setAttribute('src', 'foto-jovem-f.png')
+            } else if (idade < 55) {
+                // ADULTO
+                img.setAttribute('src', 'foto-adulto-f.png')
+            } else {
+                // IDOSA
+                img.setAttribute('src', 'foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
